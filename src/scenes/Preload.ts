29c24defac8aa52c647990a8a9017135d9c01222
Rafael <rafael_guliev94@list.ru>
2023-07-19 15:29:22 +0300

    fix paths

--- src/scenes/Preload.ts
+++ src/scenes/Preload.ts
@@ -1,24 +1,23 @@
-// @ts-nocheck
 import Phaser from "phaser";
 
-import LizardPng from '../assets/lizard/lizard.png';
-import LizardJson from '../assets/lizard/lizard.json';
-import SkyPng from '../assets/bg/Sky.png';
-import StarPng from '../assets/star.png';
-// import PlatformPng from '../assets/platform.png';
-import PlatformPng from '../assets/world/Tile-1.jpg';
-import MountainsPng from '../assets/bg/Mountains.png';
-import MiddlePng from '../assets/bg/Middle.png';
-import ForeGroundPng from '../assets/bg/foreground.png';
-// import ground1 from '../assets/bg/Ground_01.png';
-// import ground2 from '../assets/bg/Ground_02.png';
+// import LizardPng from 'lizard/lizard.png';
+// import LizardJson from 'lizard/lizard.json';
+// import SkyPng from 'bg/Sky.png';
+// import StarPng from 'star.png';
+// import PlatformPng from 'platform.png';
+// import PlatformPng from 'world/Tile-1.jpg';
+// import MountainsPng from 'bg/Mountains.png';
+// import MiddlePng from 'bg/Middle.png';
+// import ForeGroundPng from 'bg/foreground.png';
+// import ground1 from 'bg/Ground_01.png';
+// import ground2 from 'bg/Ground_02.png';
 
-import Lvl1 from '../assets/tilemap/desert.json';
-import Desert from '../assets/tilemap/desert_tiles.png';
-import Dungeon from '../assets/tilemap/dungeon.json';
+// import Lvl1 from '../../public/assets/tilemap/desert.json';
+// import Desert from 'tilemap/desert_tiles.png';
+// import Dungeon from '../../public/assets/tilemap/dungeon.json';
 
-import shadowJson from '../assets/spine/shadow.json';
-import shadowAtlas from '../assets/spine/shadow.atlas';
+// import shadowJson from '../../public/assets/spine/shadow.json';
+// import shadowAtlas from '../../public/assets/spine/shadow.atlas';
 
 export default class Preloader extends Phaser.Scene {
     constructor() {
@@ -26,21 +25,21 @@ export default class Preloader extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('tiles', Desert);
-        this.load.tilemapTiledJSON('desert', Lvl1);
-        this.load.tilemapTiledJSON('dungeon', Dungeon);
+        this.load.image('tiles', 'assets/tilemap/desert_tiles.png');
+        this.load.tilemapTiledJSON('desert', 'assets/tilemap/desert.json');
+        this.load.tilemapTiledJSON('dungeon', 'assets/tilemap/dungeon.json');
 
-        this.load.image('sky', SkyPng);
-        this.load.image('mountains', MountainsPng)
-        this.load.image('middle', MiddlePng)
-        this.load.image('foreground', ForeGroundPng)
+        this.load.image('sky', 'assets/bg/Sky.png');
+        this.load.image('mountains', 'assets/bg/Mountains.png');
+        this.load.image('middle', 'assets/bg/Middle.png')
+        this.load.image('foreground', 'assets/bg/Foreground.png')
         // this.load.image('ground1', ground1)
 
 
-        this.load.spine('shadow', shadowJson, shadowAtlas);
-        this.load.atlas('lizard', LizardPng, LizardJson);
-        this.load.image('ground', PlatformPng);
-        this.load.image('star', StarPng);
+        this.load.spine('shadow', 'assets/spine/shadow.json', 'assets/spine/shadow.atlas');
+        this.load.atlas('lizard', 'assets/lizard/lizard.png', 'assets/lizard/lizard.json');
+        this.load.image('ground', 'assets/world/Tile-1.jpg');
+        this.load.image('star', 'assets/star.png');
     }
 
     create() {
